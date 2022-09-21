7b7645d45f3ab48839c68319ce2101b17b8b41c6
JefferGonzalez <jefferd2016@gmail.com>
2022-09-21 18:50:48 -0500

    Feat: Add Spring Boot project

---
+++ src/main/java/motocycle_rental/motocycle_rental/MotocycleRentalApplication.java
@@ -0,0 +1,13 @@
+package motocycle_rental.motocycle_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotocycleRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotocycleRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/motocycle_rental/motocycle_rental/MotocycleRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package motocycle_rental.motocycle_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotocycleRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
