afc335cb39199b9fc7deaf697f42ff87804d23d7
Miguel Castro <miguelangelcmoreno@gmail.com>
2022-10-05 20:47:11 -0500

    Add relations at message entity

--- src/main/java/motocycle_rental/models/Message.java
+++ src/main/java/motocycle_rental/models/Message.java
@@ -8,17 +8,25 @@ public class Message {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
+    @Column(name = "id")
     private Integer idMessage;
-    @Column(length=250, name="message_text")
+    @Column(length = 250, name = "message_text")
     private String messageText;
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+    @ManyToOne
+    @JoinColumn(name = "motorbike_id")
+    private Motorbike motorbike;
 
     public Message() {
     }
 
-    public Message(Integer idMessage, String messageText) {
+    public Message(Integer idMessage, String messageText, Client client, Motorbike motorbike) {
         this.idMessage = idMessage;
         this.messageText = messageText;
+        this.client = client;
+        this.motorbike = motorbike;
     }
 
     public Integer getIdMessage() {
@@ -37,4 +45,19 @@ public class Message {
         this.messageText = messageText;
     }
 
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Motorbike getMotorbike() {
+        return motorbike;
+    }
+
+    public void setMotorbike(Motorbike motorbike) {
+        this.motorbike = motorbike;
+    }
 }
