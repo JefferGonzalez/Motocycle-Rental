c1e7dcbeaeb26598f78f3acaad97a2196ef46e3b
mariaperezr <mariapaulaperezrodriguez62@gmail.com>
2022-10-07 11:53:12 -0500

    Create score controller

---
+++ src/main/java/motocycle_rental/controllers/ScoreController.java
@@ -0,0 +1,30 @@
+package motocycle_rental.controllers;
+
+import motocycle_rental.models.Score;
+import motocycle_rental.repositories.ScoreInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Score")
+public class ScoreController {
+
+        @Autowired
+        private ScoreInterface scoreInterface;
+
+        @GetMapping("/all")
+        public List<Score> getAll(){
+            return (List<Score>) scoreInterface.findAll();
+        }
+
+        @PostMapping("/save")
+        @ResponseStatus(HttpStatus.CREATED)
+        public Score save(@RequestBody Score score){
+            return scoreInterface.save(score);
+        }
+
+    }
+
