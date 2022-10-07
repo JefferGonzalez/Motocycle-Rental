f7ea6098dee68ff46735c646acd1d97c4bc18e8c
JefferGonzalez <jefferd2016@gmail.com>
2022-10-07 12:07:41 -0500

    Add Message Controller

---
+++ src/main/java/motocycle_rental/controllers/MessageController.java
@@ -0,0 +1,29 @@
+package motocycle_rental.controllers;
+
+import motocycle_rental.models.Message;
+import motocycle_rental.repositories.MessageInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Message")
+public class MessageController {
+
+    @Autowired
+    private MessageInterface messageInterface;
+
+    @GetMapping("/all")
+    public List<Message> getAll(){
+        return (List<Message>) messageInterface.findAll();
+    }
+
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Message save(@RequestBody Message message){
+        return messageInterface.save(message);
+    }
+
+}
